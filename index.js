1660396db41766880bdc5032424630024a761592
Diego Acosta <diegonake@gmail.com>
2015-09-27 09:50:59 -0300

    moar

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var cpu = require('./lib/cpu');
+var Gameboy = require('./lib/gameboy')();
+var Cart = require('./lib/cart')('./roms/tetris.gb');
 
-cpu.loadFile('./roms/tetris.gb');
-cpu.start();
+Gameboy.insertCart(Cart);
+Gameboy.powerOn();
