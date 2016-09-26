ff022d5295edf215a10d2116556c67f88e3cc758
Diego Acosta <diegonake@gmail.com>
2016-09-26 16:32:58 +0200

    support for booting up without bios

--- example/script.js
+++ example/script.js
@@ -1,15 +1,15 @@
 'use strict';
 
 const fs = require('fs');
+const cart = fs.readFileSync('./roms/opus5.gb');
+const bios = fs.readFileSync('./support/bios.bin');
 const Gameboy = require('../');
 
 const gameboy = new Gameboy();
-gameboy.loadCart(fs.readFileSync('./roms/opus5.gb'));
+gameboy.loadCart(cart);
 gameboy.powerOn();
 
-
 let i = 0;
-
 gameboy.gpu.on('frame', (canvas) => {
     if (++i % 60) return;
     fs.writeFile(`./screenshot/${i / 60}.png`, canvas.toBuffer());
