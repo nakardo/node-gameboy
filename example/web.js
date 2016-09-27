151d7f95f72844a71537509ac9d25a4123765ed3
Diego Acosta <diegonake@gmail.com>
2016-09-27 16:48:59 +0200

    timer

--- example/web.js
+++ example/web.js
@@ -1,12 +1,14 @@
 'use strict';
 
 const fs = require('fs');
+const cart = fs.readFileSync('./roms/opus5.gb');
+const bios = fs.readFileSync('./support/bios.bin');
 const Gameboy = require('../');
 
 
 localStorage.debug = 'gpu';
 
-const gameboy = new Gameboy();
+const gameboy = new Gameboy({ bios });
 gameboy.loadCart(fs.readFileSync('./roms/opus5.gb'));
 gameboy.powerOn();
 
