bc60b314606353a19d0cf49b7432ed1548297386
Diego Acosta <diegonake@gmail.com>
2016-09-30 00:09:52 +0200

    mmu echo ram and extra opcodes

--- example/script.js
+++ example/script.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const fs = require('fs');
-const cart = fs.readFileSync('./roms/07-jr,jp,call,ret,rst.gb');
+const cart = fs.readFileSync('./roms/opus5.gb');
 const bios = fs.readFileSync('./support/bios.bin');
 const Gameboy = require('../');
 
 
-const gameboy = new Gameboy();
+const gameboy = new Gameboy(bios);
 gameboy.loadCart(cart);
 gameboy.powerOn();
 
