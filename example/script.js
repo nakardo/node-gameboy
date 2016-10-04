afe910541ec50bf789c20ca6bc03029111d78e6f
Diego Acosta <diegonake@gmail.com>
2016-10-04 16:35:56 +0200

    finish refactor but not working

--- example/script.js
+++ example/script.js
@@ -1,17 +1,17 @@
 'use strict';
 
 const fs = require('fs');
-const cart = fs.readFileSync('./roms/01-special.gb');
+const cart = fs.readFileSync('./roms/opus5.gb');
 const bios = fs.readFileSync('./support/bios.bin');
 const Gameboy = require('../');
 
 
-const gameboy = new Gameboy();
+const gameboy = new Gameboy(bios);
 gameboy.loadCart(cart);
 gameboy.powerOn();
 
-// let i = 0;
-// gameboy.gpu.on('frame', (canvas) => {
-//     if (++i % 60) return;
-//     fs.writeFile(`./screenshot/${i / 60}.png`, canvas.toBuffer());
-// });
+let i = 0;
+gameboy.gpu.on('frame', (canvas) => {
+    if (++i % 60) return;
+    fs.writeFile(`./screenshot/${i / 60}.png`, canvas.toBuffer());
+});
