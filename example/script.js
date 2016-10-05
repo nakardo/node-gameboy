09164f75ba84d3c455ce09cc1eecfe0f7a6e6da2
Diego Acosta <diegonake@gmail.com>
2016-10-05 16:44:50 +0200

    move flags log

--- example/script.js
+++ example/script.js
@@ -1,5 +1,26 @@
 'use strict';
 
+/**
+ * Tests:
+ *
+ * Passed
+ * - 03-op sp,h.gb
+ * - 04-op r,imm.gb
+ * - 05-op rp.gb
+ * - 06-ld r,r.gb
+ * - 10-bit ops.gb
+ *
+ * Failed
+ * - 01-special.gb
+ * - 02-interrupts.gb
+ * - 08-misc instrs.gb
+ * - 09-op r,r.gb
+ * - 11-op a,(hl).gb
+ *
+ * Not working:
+ * - 07-jr,jp,call,ret,rst.gb
+ */
+
 const fs = require('fs');
 const cart = fs.readFileSync('./roms/opus5.gb');
 const bios = fs.readFileSync('./support/bios.bin');
