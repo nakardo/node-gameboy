8552675646296af74260bf63e273f7cd62e98352
Diego Acosta <diegonake@gmail.com>
2016-10-05 15:00:18 +0200

    rework mmu

--- example/script.js
+++ example/script.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const fs = require('fs');
-const cart = fs.readFileSync('./roms/tetris.gb');
+const cart = fs.readFileSync('./roms/opus5.gb');
 const bios = fs.readFileSync('./support/bios.bin');
 const Gameboy = require('../');
 
 
-const gameboy = new Gameboy();
+const gameboy = new Gameboy(bios);
 gameboy.loadCart(cart);
 gameboy.powerOn();
 
