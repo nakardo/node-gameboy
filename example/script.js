1e2fffdae4c321b9fd8fbc8d263f5e45164ffcdc
Diego Acosta <diegonake@gmail.com>
2016-10-13 22:32:10 +0200

    fix af

--- example/script.js
+++ example/script.js
@@ -4,23 +4,23 @@
  * Tests:
  *
  * Passed
+ * - 01-special.gb
  * - 03-op sp,hl.gb
  * - 04-op r,imm.gb
  * - 05-op rp.gb
  * - 06-ld r,r.gb
  * - 07-jr,jp,call,ret,rst.gb
+ * - 08-misc instrs.gb
  * - 09-op r,r.gb
  * - 10-bit ops.gb
  * - 11-op a,(hl).gb
  *
  * Failed
- * - 01-special.gb
  * - 02-interrupts.gb
- * - 08-misc instrs.gb
  */
 
 const fs = require('fs');
-const cart = fs.readFileSync('./roms/01-special.gb');
+const cart = fs.readFileSync('./roms/11-op a,(hl).gb');
 const bios = fs.readFileSync('./support/bios.bin');
 const Gameboy = require('../');
 
