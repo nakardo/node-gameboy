4e59865c5f0c9147c8a30b933bc9364cb848c7bc
Diego Acosta <diegonake@gmail.com>
2016-10-14 10:03:52 +0200

    add debug statement for rom test out

--- example/script.js
+++ example/script.js
@@ -4,23 +4,24 @@
  * Tests:
  *
  * Passed
- * - 01-special.gb
- * - 03-op sp,hl.gb
- * - 04-op r,imm.gb
- * - 05-op rp.gb
- * - 06-ld r,r.gb
- * - 07-jr,jp,call,ret,rst.gb
- * - 08-misc instrs.gb
- * - 09-op r,r.gb
- * - 10-bit ops.gb
- * - 11-op a,(hl).gb
+ * - /cpu_instrs/individual/01-special.gb
+ * - /cpu_instrs/individual/03-op sp,hl.gb
+ * - /cpu_instrs/individual/04-op r,imm.gb
+ * - /cpu_instrs/individual/05-op rp.gb
+ * - /cpu_instrs/individual/06-ld r,r.gb
+ * - /cpu_instrs/individual/07-jr,jp,call,ret,rst.gb
+ * - /cpu_instrs/individual/08-misc instrs.gb
+ * - /cpu_instrs/individual/09-op r,r.gb
+ * - /cpu_instrs/individual/10-bit ops.gb
+ * - /cpu_instrs/individual/11-op a,(hl).gb
  *
  * Failed
- * - 02-interrupts.gb
+ * - /cpu_instrs/individual/02-interrupts.gb
+ * - /instr_timing/instr_timing.gb
  */
 
 const fs = require('fs');
-const cart = fs.readFileSync('./roms/11-op a,(hl).gb');
+const cart = fs.readFileSync('./roms/cpu_instrs/individual/02-interrupts.gb');
 const bios = fs.readFileSync('./support/bios.bin');
 const Gameboy = require('../');
 
