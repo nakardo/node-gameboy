305e496656088cefdb84e795eff2b8387549bcae
Diego Acosta <diegonake@gmail.com>
2016-10-20 16:16:01 +0200

    sum bytes

--- example/script.js
+++ example/script.js
@@ -4,20 +4,21 @@
  * Tests:
  *
  * Passed
- * - /cpu_instrs/individual/01-special.gb
- * - /cpu_instrs/individual/03-op sp,hl.gb
- * - /cpu_instrs/individual/04-op r,imm.gb
- * - /cpu_instrs/individual/05-op rp.gb
- * - /cpu_instrs/individual/06-ld r,r.gb
- * - /cpu_instrs/individual/07-jr,jp,call,ret,rst.gb
- * - /cpu_instrs/individual/08-misc instrs.gb
- * - /cpu_instrs/individual/09-op r,r.gb
- * - /cpu_instrs/individual/10-bit ops.gb
- * - /cpu_instrs/individual/11-op a,(hl).gb
+ * - /roms/cpu_instrs/individual/01-special.gb
+ * - /roms/cpu_instrs/individual/03-op sp,hl.gb
+ * - /roms/cpu_instrs/individual/04-op r,imm.gb
+ * - /roms/cpu_instrs/individual/05-op rp.gb
+ * - /roms/cpu_instrs/individual/06-ld r,r.gb
+ * - /roms/cpu_instrs/individual/07-jr,jp,call,ret,rst.gb
+ * - /roms/cpu_instrs/individual/08-misc instrs.gb
+ * - /roms/cpu_instrs/individual/09-op r,r.gb
+ * - /roms/cpu_instrs/individual/10-bit ops.gb
+ * - /roms/cpu_instrs/individual/11-op a,(hl).gb
+ * - /roms/cpu_instrs/cpu_instrs.gb
  *
  * Failed
- * - /cpu_instrs/individual/02-interrupts.gb
- * - /instr_timing/instr_timing.gb
+ * - /roms/cpu_instrs/individual/02-interrupts.gb
+ * - /roms/instr_timing/instr_timing.gb
  */
 
 const fs = require('fs');
@@ -26,7 +27,7 @@ const bios = fs.readFileSync('./support/bios.bin');
 const Gameboy = require('../');
 
 
-const gameboy = new Gameboy(cart);
+const gameboy = new Gameboy(cart, bios);
 gameboy.powerOn();
 
 // let i = 0;
