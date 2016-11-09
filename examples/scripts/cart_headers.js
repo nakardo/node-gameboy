b1de3ee58db9f95e3fc2c44828914b9b59f3c9e5
Diego Acosta <diegonake@gmail.com>
2016-11-09 13:21:40 +0100

    fix cart require

--- examples/scripts/cart_headers.js
+++ examples/scripts/cart_headers.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const fs = require('fs');
-const Cart = require('../../lib/cart');
+const Cart = require('../../lib/io/cart');
 
 
 const cart = new Cart(fs.readFileSync('./roms/pokemon.gb'));
