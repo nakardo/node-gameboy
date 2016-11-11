b8eaff46f4c36a9b3659eb78c7694d4bbcb1de39
Diego Acosta <diegonake@gmail.com>
2016-11-11 17:35:55 +0100

    add rendering and scale buttons

--- examples/web.js
+++ examples/web.js
@@ -7,6 +7,15 @@ localStorage.debug = '';
 
 const gameboy = new Gameboy();
 
+// Render
+
+const canvas = document.getElementById('frame');
+const ctx = canvas.getContext('2d');
+
+gameboy.gpu.on('frame', (offcanvas) => {
+    ctx.drawImage(offcanvas, 0, 0);
+});
+
 // Buttons
 
 const inputAction = document.getElementById('input');
@@ -28,16 +37,21 @@ inputAction.addEventListener('change', loadFile);
 pauseAction.addEventListener('click', () => gameboy.pauseResume());
 resetAction.addEventListener('click', () => gameboy.reset());
 
+const renderingAction = document.getElementById('rendering');
+const scaleAction = document.getElementById('scale');
+
+renderingAction.addEventListener('click', () => {
+    $(canvas).toggleClass('pixelated');
+});
+
+const sizes = ['160px', '320px', '640px'];
+
+scaleAction.addEventListener('click', () => {
+    let idx = sizes.indexOf(canvas.style.width || '320px');
+    canvas.style.width = sizes[++idx % 3];
+});
+
 // Joypad
 
 document.addEventListener('keydown', (e) => gameboy.joypad.keyDown(e.keyCode));
 document.addEventListener('keyup', (e) => gameboy.joypad.keyUp(e.keyCode));
-
-// Render
-
-const canvas = document.getElementById('frame');
-const ctx = canvas.getContext('2d');
-
-gameboy.gpu.on('frame', (offcanvas) => {
-    ctx.drawImage(offcanvas, 0, 0);
-});
