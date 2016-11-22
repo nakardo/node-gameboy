9c73deafe82480de2b37c883eb516263b62249a5
Diego Acosta <diegonake@gmail.com>
2016-11-22 18:20:16 +0100

    use fullscreen for gh-pages example

--- examples/gh-pages/app.js
+++ examples/gh-pages/app.js
@@ -1,21 +1,17 @@
-var gameboy = window.gameboy = new (window.Gameboy)();
-
 // localStorage.debug = '';
+
+var gameboy = window.gameboy = new (window.Gameboy)();
 
 // Render
 
 var canvas = document.getElementById('frame');
-var ctx = canvas.getContext('2d');
 
+var ctx = canvas.getContext('2d');
 gameboy.gpu.on('frame', function (offcanvas) {
     ctx.drawImage(offcanvas, 0, 0);
 });
 
 // Buttons
-
-var inputAction = document.getElementById('input');
-var pauseAction = document.getElementById('pause');
-var resetAction = document.getElementById('reset');
 
 function loadFile () {
     if (!this.files.length) return;
@@ -28,29 +24,23 @@ function loadFile () {
     reader.readAsArrayBuffer(this.files[0]);
 }
 
-inputAction.addEventListener('change', loadFile);
-pauseAction.addEventListener('click', function () { gameboy.pauseResume(); });
-resetAction.addEventListener('click', function () { gameboy.reset(); });
+function fullscreen () {
+    (
+        canvas.requestFullscreen ||
+        canvas.mozRequestFullScreen ||
+        canvas.webkitRequestFullscreen ||
+        canvas.msRequestFullscreen
+    )
+    .call(canvas);
+}
 
-var renderingAction = document.getElementById('rendering');
-var scaleAction = document.getElementById('scale');
-
-renderingAction.addEventListener('click', function () {
-    $(canvas).toggleClass('pixelated');
-});
-
-var sizes = ['160px', '320px', '640px'];
-
-scaleAction.addEventListener('click', function () {
-    var idx = sizes.indexOf(canvas.style.width || '320px');
-    canvas.style.width = sizes[++idx % 3];
-});
+$('#input').change(loadFile);
+$('#fullscreen').click(fullscreen);
+$('#pause').click(function () { gameboy.pauseResume() });
+$('#reset').click(function () { gameboy.reset() });
+$('#rendering').click(function () { $('#frame').toggleClass('pixelated') });
 
 // Joypad
 
-document.addEventListener('keydown', function (e) {
-    gameboy.joypad.keyDown(e.keyCode);
-});
-document.addEventListener('keyup', function (e) {
-    gameboy.joypad.keyUp(e.keyCode);
-});
+$(document).keydown(function (e) { gameboy.joypad.keyDown(e.keyCode) });
+$(document).keyup(function (e) { gameboy.joypad.keyUp(e.keyCode) });
